2c00e8fa6f8cba085cdb7ea22af235aa6db93dcb
aledebarba <ale@alemacedo.com>
2018-01-31 16:00:40 -0200

    Alterações de layout na tela de listagem de notas, alterações de estilo, inclusão de launch icon para Android e UWP

--- XF.VKNG.Notes/XF.VKNG.Notes.UWP/Properties/AssemblyInfo.cs
+++ XF.VKNG.Notes/XF.VKNG.Notes.UWP/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XF.VKNG.Notes.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TypeX")]
+[assembly: AssemblyDescription("TypeX é um aplicativo simples e rápido para criar anotações protegidas por senhas.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("VKNG Studio")]
 [assembly: AssemblyProduct("XF.VKNG.Notes.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("VKNG e TypeX são marcas registradas.")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
